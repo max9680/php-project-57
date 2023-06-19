b7305bf3b676ab19a789773231a57f230d68edf1
Maxim <max9680@mail.ru>
2023-06-19 12:31:21 +0300

    php artisan breeze:install

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-
-    flash('Message')->important();
-
     return view('welcome');
 });
 
@@ -31,4 +28,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
