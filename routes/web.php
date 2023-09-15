4f0fa989ea4580c237c45e8e6ea3d5c94eaf7b1a
max9680@mail.ru <max9680@mail.ru>
2023-09-15 14:53:33 +0300

    add TaskStatus model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskStatus\TaskStatusController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+Route::resource('taskStatus', TaskStatusController::class);
+
