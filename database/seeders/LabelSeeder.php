8843c4c7fbd9e066ff8e866645496a64c57e9f5e
max9680@mail.ru <max9680@mail.ru>
2023-10-06 15:52:19 +0300

    add LabelSeeder

--- database/seeders/LabelSeeder.php
+++ database/seeders/LabelSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class LabelSeeder extends Seeder
@@ -14,6 +15,11 @@ class LabelSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Label::factory(4)->state(new Sequence(
+            ['name' => 'ошибка', 'description' => 'Какая-то ошибка в коде или проблема с функциональностью	'],
+            ['name' => 'документация', 'description' => 'Задача которая касается документации'],
+            ['name' => 'дубликат', 'description' => 'Повтор другой задачи'],
+            ['name' => 'доработка', 'description' => 'Новая фича, которую нужно запилить'],
+            ))->create();
     }
 }
