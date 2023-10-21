474c8e97135dded43b12e61d66360a7651e4dc23
max9680@mail.ru <max9680@mail.ru>
2023-10-21 12:44:10 +0300

    change routes to Resource routes

--- routes/web.php
+++ routes/web.php
@@ -33,47 +33,30 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
-//Route::resource('task_statuses', TaskStatusController::class);
 
-Route::get('/task_statuses', [TaskStatusController::class, 'index'])->name('task_statuses.index');
-Route::get('/task_statuses/create', [TaskStatusController::class, 'create'])
-    ->name('task_statuses.create')
-    ->middleware('auth');
-Route::post('/task_statuses', [TaskStatusController::class, 'store'])->name('task_statuses.store');
-Route::get('/task_statuses/{task_status}/edit', [TaskStatusController::class, 'edit'])
-    ->name('task_statuses.edit')
-    ->middleware('auth');
-Route::patch('/task_statuses/{task_status}', [TaskStatusController::class, 'update'])
-    ->name('task_statuses.update')
-    ->middleware('auth');
-Route::delete('/task_statuses/{task_status}', [TaskStatusController::class, 'destroy'])
-    ->name('task_statuses.destroy')
-    ->middleware('auth');
+Route::resource('task_statuses', TaskStatusController::class)->except([
+    'index'
+])->middleware('auth');
 
-//Route::resource('task', TaskController::class);
-
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create')
-    ->middleware('auth');
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store')
-    ->middleware('auth');
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
-Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit')
-    ->middleware('auth');
-Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update')
-    ->middleware('auth');
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])
-    ->name('tasks.destroy');
+Route::resource('task_statuses', TaskStatusController::class)->only([
+    'index'
+]);
 
 
-Route::post('/labels', [LabelController::class, 'store'])->name('labels.store')
-    ->middleware('auth');
-Route::get('/labels', [LabelController::class, 'index'])->name('labels.index');
-Route::get('/labels/create', [LabelController::class, 'create'])->name('labels.create')
-    ->middleware('auth');
-Route::get('/labels/{label}/edit', [LabelController::class, 'edit'])->name('labels.edit')
-    ->middleware('auth');
-Route::delete('/labels/{label}', [LabelController::class, 'destroy'])->name('labels.destroy')
-    ->middleware('auth');
-Route::patch('/labels/{label}', [LabelController::class, 'update'])->name('labels.update')
-    ->middleware('auth');
+Route::resource('tasks', TaskController::class)->except([
+    'index', 'show'
+])->middleware('auth');
+
+Route::resource('tasks', TaskController::class)->only([
+    'index', 'show'
+]);
+
+
+Route::resource('labels', LabelController::class)->except([
+    'index'
+])->middleware('auth');
+
+Route::resource('labels', LabelController::class)->only([
+    'index'
+]);
+
