89f2a03f67f7b756bd5b7e9dac0967d6b3dedf2a
max9680@mail.ru <max9680@mail.ru>
2023-10-29 13:43:07 +0300

    refactoring TaskStatusController

--- app/Http/Controllers/TaskStatus/TaskStatusController.php
+++ app/Http/Controllers/TaskStatus/TaskStatusController.php
@@ -57,17 +57,6 @@ class TaskStatusController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
