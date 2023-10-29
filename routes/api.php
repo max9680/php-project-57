e00b8e03677f619a63c093064066ce200adaba8c
max9680@mail.ru <max9680@mail.ru>
2023-10-29 15:46:54 +0300

    refactoring routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\TaskStatus\TaskStatusController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('task_statuses', TaskStatusController::class)->only('index', 'store', 'update', 'destroy');
