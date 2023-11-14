cea6ed3ede045ae9f7a9e7ab5beda2dcc3a91421
m.vorobyev <max9680@mail.ru>
2023-11-14 17:43:12 +0300

    adobe for smartphone

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -18,19 +18,18 @@
         <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 mx-auto">
             <a href="{{ route('home') }}" class="flex items-center">
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ __('strings.task manager') }}</span></a>
-            @auth
-                <div class="flex items-center lg:order-2">
+            <div class="flex items-center sm:order-1 md:order-2">
+                @auth
                     <a data-method="post" href="{{ route('logout') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('strings.log out') }}</a>
-                </div>
-            @else
-                <div class="flex items-center lg:order-2">
-                <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('strings.log in') }}</a>
-                <a href="{{ route('register') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">{{ __('strings.registration') }}</a>
+                @else
+                    <a href="{{ route('login') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ __('strings.log in') }}</a>
+                    <a href="{{ route('register') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">{{ __('strings.registration') }}</a>
+                @endauth
             </div>
-            @endauth
 
-            <div class="items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            {{--<div class="flex flex-wrap items-center justify-between hidden w-full lg:flex lg:w-auto lg:order-1">--}}
+            <div class="flex flex-wrap justify-between max-w-screen-xl lg:w-auto sm:order-2 md:order-1">
+                <ul class="flex flex-row mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li><a href="{{ route('tasks.index') }}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">{{ __('strings.tasks') }}</a></li>
                     <li><a href="{{ route('task_statuses.index') }}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">{{ __('strings.statuses') }}</a></li>
                     <li><a href="{{ route('labels.index') }}" class="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 lg:p-0">{{ __('strings.labels') }}</a></li>
