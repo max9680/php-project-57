96d51dec635d90646203a0809875b04104968a25
m.vorobyev <max9680@mail.ru>
2023-11-14 17:48:54 +0300

    adobe for smartphone-2

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,7 +40,7 @@
 </header>
 
 <section class="bg-white dark:bg-gray-900">
-    <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
+    <div class="grid max-w-screen-xl px-4 pt-40 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
 
 
         @if (flash()->message)
